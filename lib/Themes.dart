0d70a9a3c88bd9bd0831c894276d438733148515
ezorrio <ezorrio@gmail.com>
2021-04-18 13:10:52 +0200

    Changed color to cyan

--- lib/Themes.dart
+++ lib/Themes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppColors {
-  static var primary = Colors.teal;
+  static var primary = Colors.cyan;
 }
 
 class Themes {
